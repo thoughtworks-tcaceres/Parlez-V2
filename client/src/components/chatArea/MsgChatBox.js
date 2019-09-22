0b5880908c385d5d58153629b65ff2177b8e2071
tylerccaceres <tyler.caceres@gmail.com>
2019-09-22 13:45:00 -0400

    msg box

--- client/src/components/chatArea/MsgChatBox.js
+++ client/src/components/chatArea/MsgChatBox.js
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
-import { InputGroup, FormControl } from "react-bootstrap";
 
 const MsgChatBox = () => {
+  const [msgContent, setMsgContent] = useState("");
+
+  const msgContentChange = e => {
+    setMsgContent(e.target.value);
+    console.log(msgContent);
+  };
+
   return (
     <div>
-      <textarea placeholder="Enter Message Here"></textarea>
+      <textarea
+        placeholder="Enter Message ..."
+        onChange={msgContentChange}
+        value={msgContent}
+        maxLength={255}
+        rows={5}
+      ></textarea>
+      <i class="far fa-paper-plane fa-3x"></i>
     </div>
   );
 };
