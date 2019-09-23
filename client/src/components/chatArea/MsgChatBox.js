0a98ae6438aeca699132710ffd019cd6ba4e3948
tylerccaceres <tyler.caceres@gmail.com>
2019-09-22 21:36:36 -0400

    msg chat box changes

--- client/src/components/chatArea/MsgChatBox.js
+++ client/src/components/chatArea/MsgChatBox.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./MsgChatBox.scss";
 
 const MsgChatBox = () => {
   const [msgContent, setMsgContent] = useState("");
@@ -8,18 +9,27 @@ const MsgChatBox = () => {
     console.log(msgContent);
   };
 
+  const submitMessage = () => {
+    if (msgContent.length > 0) {
+      //send message
+    }
+    setMsgContent("");
+  };
+
   return (
     <div>
       <textarea
+        id="msgText"
         placeholder="Enter Message ..."
         onChange={msgContentChange}
+        onKeyUp={keyUpHandler}
         value={msgContent}
         maxLength={255}
         rows={5}
         autoFocus
         style={{ resize: "none" }}
       ></textarea>
-      <i className="far fa-paper-plane fa-3x"></i>
+      <i className="submitBtn far fa-paper-plane fa-3x" onClick={submitMessage}></i>
     </div>
   );
 };
